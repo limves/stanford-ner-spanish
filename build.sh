5e0856abb9e06e1e20f3a88136f514ab26c5795a
Lim <limves@gmail.com>
2016-09-10 20:34:23 -0500

    example of micelanea works

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-tika --text  http://www.sat.gob.mx/informacion_fiscal/normatividad/Documents/3armf2016_1aparte_14072016.doc > 3armf2016_1aparte.txt
+#tika --text  http://www.sat.gob.mx/informacion_fiscal/normatividad/Documents/3armf2016_1aparte_14072016.doc > 3armf2016_1aparte.txt
 
 java -cp stanford-ner.jar edu.stanford.nlp.process.PTBTokenizer 3armf2016_1aparte.txt > 3armf2016_1aparte.tok
 perl -ne 'chomp; print "$_\tO\n"' 3armf2016_1aparte.tok > 3armf2016_1aparte.tsv
-java -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop 3armf2016_1aparte.prop
+java -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop 3armf2016.prop
 
-#java -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model.3armf2016.ser.gz  -testFile 3armf2016_1aparte_14072016.doc.txt.1
+java -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier ner-model.3armf2016.ser.gz  -testFile 3armf2016_1aparte.txt
+
+# Java
+javac -cp "stanford-ner.jar:." NERDemoES.java
+java -cp "stanford-ner.jar:." NERDemoES
--- clean.sh
+++ clean.sh
@@ -3,4 +3,8 @@
 # Create CRFClassifier
 rm austen.prop
 rm jane-austen-emma-ch*.*
-rm 3armf2016_1aparte.*
+rm 3armf2016_1aparte.tok
+rm 3armf2016_1aparte.tsv 
+rm ner-model.3armf2016.ser.gz
+
+rm *.class
